b8d667924f88340cdc500b80fee3ea1916b75d48
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2023-09-24 19:33:10 +0100

    Header, button components, brief props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
 
-const Header = () => {
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click');
+  };
+
   return (
-    <header>
-      <h1>Task tracker</h1>
+    <header className='header'>
+      <h1> {title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   );
+};
+
+Header.defaultProps = {
+  title: 'Task Tracker ',
+};
+Header.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Header;
